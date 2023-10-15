d8f9cf4e4891f30cc07d673fcd8ef5374585487c
kadir <kadirdmn38@gmail.com>
2023-10-16 01:45:45 +0300

    Update

--- DortVeBesKuvveti/src/DortVeBesKuvveti.java
+++ DortVeBesKuvveti/src/DortVeBesKuvveti.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class DortVeBesKuvveti {
+    public static void main(String[] args) {
+        int num;
+        Scanner scan = new Scanner(System.in);
+
+        // 0' dan büyük tam sayı girilmesi.
+        do{
+            System.out.print("Bir sayı giriniz. ");
+            num = scan.nextInt();
+            System.out.println("Girilen sayı : "+num);
+        }while(num<0);
+
+
+        // 4' ün katlarını yazdıran kısım.
+        System.out.println("\n4' ün katları. ");
+        for(int i=1;i<=num;i*=4){
+            System.out.println(i);
+        }
+
+        // 5' in katlarını yazdıran kısım.
+        System.out.println("\n5' in katları. ");
+        for(int i=1;i<=num;i*=5){
+            System.out.println(i);
+        }
+    }
 }
